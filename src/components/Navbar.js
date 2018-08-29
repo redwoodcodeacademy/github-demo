ae9ed32238ac62f1479364cd0e41752b07b5f532
Harrison Spain <harrison.spain@redwoodcodeacademy.com>
2018-08-29 11:38:16 -0700

    Added Navbar component. Updated app.js welcome message

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                h3llo
+            </div>
+         );
+    }
+}
+ 
+export default Navbar;
